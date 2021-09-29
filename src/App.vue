615f8edf50838dabd4a458f62d8a8e9a671fb416
gfrn <guifreitas89@hotmail.com>
2021-09-29 16:20:59 -0300

    Fixes camel case on snackbar

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,7 @@
       {{ $store.state.message }}
 
       <template v-slot:action="{ attrs }">
-        <v-btn icon text v-bind="attrs" @click="$store.commit('hideSnackbar')">
+        <v-btn icon text v-bind="attrs" @click="$store.commit('hide_snackbar')">
           <v-icon>mdi-close</v-icon>
         </v-btn>
       </template>
